87f9b6a18cf0b952614c0a0fe91e80a3ea4dad3f
Markus Woltjer <Markus Woltjer>
2017-04-07 22:42:19 -0700

    matrix multiplication not working

---
+++ HW1/HW1.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def main():
+
+    #load training data
+    train_data = np.loadtxt("housing_train")
+
+    #load testing data
+    test_data = np.matrix(np.loadtxt("housing_test"))
+
+    #split training data into attributes and labels
+    Y = train_data[:,-1]
+    X_no_dummy = train_data[:,0:-1]
+
+    #prepend dummy column for "b multipliers"
+    dummy = np.ones((len(X_no_dummy), 1))
+    X = np.concatenate((dummy, X_no_dummy), 1)
+
+    #compute optimal weights
+    w = (X.T*X).I*(X.T*Y)
+
+
+    print(X)
+
+main()
