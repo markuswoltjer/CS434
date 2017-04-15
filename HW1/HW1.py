0fd934d2a4ba1dd7f2c6f0997d80a64d409ed9ab
Markus Woltjer <Markus Woltjer>
2017-04-14 18:24:27 -0700

    add file

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -1,6 +1,6 @@
 import numpy as np
 import lin_reg as lr
-import matplotlib.pyplot as plt
+import pylab as pl
 
 
 def format_data(filename,dummy=False):
@@ -22,19 +22,23 @@ def main():
     #1 Load data
     (X_train,Y_train) = format_data("housing_train",dummy=True)
     (X_test, Y_test) = format_data("housing_test",dummy=True)
-    #2 compute optimal weights
+
+    #2 Compute Optimal Weights Vector
     reg_w_dum = lr.Lin_Reg(X_train,Y_train)
     print(reg_w_dum.get_w())
-    #3 compute sse
+
+    #3 Compute Standard Square Error
     print(reg_w_dum.get_sse(Y_train,X_train))
     print(reg_w_dum.get_sse(Y_test,X_test))
-    #4 Repeat w/o dummy
+
+    #4 Repeat SSE Computation without Dummy Variable
     (X_train_nod,Y_train_nod) = format_data("housing_train")
     (X_test_nod, Y_test_nod) = format_data("housing_test")
     reg_nod = lr.Lin_Reg(X_train_nod,Y_train_nod)
     print(reg_nod.get_sse(Y_train_nod,X_train_nod))
     print(reg_nod.get_sse(Y_test_nod,X_test_nod))
-    #5 Throw in random features
+
+    #5 Add Random Features
     end_p=100
     X_train_randf = X_train
     X_test_rf = X_test
@@ -42,6 +46,7 @@ def main():
     res_test_rf = np.zeros(end_p)
     res_test_emp = np.zeros(end_p)
     res_train = np.zeros(end_p)
+
     for i in range(0,end_p):
         reg = lr.Lin_Reg(X_train_randf,Y_train)
         res_train[i]=reg.get_sse(Y_train,X_train_randf)
@@ -51,20 +56,39 @@ def main():
         X_test_rf = add_rand_f(X_test_rf,i+1)
         X_test_emp=add_empty_f(X_test_emp)
 
-    plt.plot(range(0,end_p), res_train, 'r--', range(0,end_p), res_test_rf, 'bs', range(0,end_p), res_test_emp, 'g^')
-    plt.show()
-    #6
+    pl.plot(range(0,end_p), res_train, 'r--', label='Base Features')
+    pl.plot(range(0,end_p), res_test_emp, 'g^', label='With Empty Features')
+    pl.plot(range(0,end_p), res_test_rf, 'bs', label = 'With Random Features')
+
+    pl.title('Linear Regression with Various Numbers of Random Features')
+    pl.ylabel('Sum of Squared Error (SSE)')
+    pl.xlabel('Number of Random Features')
+    pl.legend(loc='center left')
+
+    pl.show()
+
+    #Variant of Linear Regression in #6
     lambs=[0, 0.01, 0.05, 0.1, 0.5, 1, 5]
     res_train = []
     res_test = []
+
     for i in lambs:
         reg = lr.Lin_Reg(X_train,Y_train, lamb=i)
         res_train.append(reg.get_sse(Y_train,X_train))
         res_test.append(reg.get_sse(Y_test,X_test))
 
-    plt.plot(lambs, res_train)
-    plt.show()
-    plt.plot(lambs, res_test)
-    plt.show()
+    pl.plot(lambs, res_train)
+    pl.title('Train Plot')
+    pl.ylabel('train y')
+    pl.xlabel('train x')
+    pl.show()
+
+
+
+    pl.plot(lambs, res_test)
+    pl.title('Test Plot')
+    pl.ylabel('test y')
+    pl.xlabel('test x')
+    pl.show()
 
 main()
