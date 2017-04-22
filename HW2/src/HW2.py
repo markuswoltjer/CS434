cd6621ab0f953e704dc44a0952f30d5280eccfed
samtjacobs <jacobssa@oregonstate.edu>
2017-04-22 01:18:12 -0700

    Added convergences

--- HW2/src/HW2.py
+++ HW2/src/HW2.py
@@ -20,12 +20,61 @@ def add_dummy(x):
     return np.concatenate((np.ones((len(x), 1)), x), 1)
 
 def main():
+    # 1
     (X_train, Y_train) = format_data_csv2('../data/usps-4-9-train.csv')
     (X_test, Y_test) = format_data_csv2('../data/usps-4-9-test.csv')
-
-    log_reg = lr.Log_Reg(add_dummy(X_train), Y_train, 'iteration', 0.1, 10)
+    for i in range(5,10):
+        log_reg = lr.Log_Reg(add_dummy(X_train), Y_train, 'objective', 10**-i, 10)
+        print("learning rate = 10^-" + str(i))
+        print("Accuracy (predictions on right side of 0.5): "
+              + str(lr.test_accuracy(log_reg.w, add_dummy(X_train), Y_train)))
+        print("Loss for training: " + str(log_reg.obj))
 #    print (log_reg.w)
 #    print(log_reg.get_prob(X_test[300]))
+
+    # 2
+    test_acc = []
+    train_acc = []
+    learn = 10**-9
+    log_reg = lr.Log_Reg(add_dummy(X_train), Y_train, 'iteration', learn, 1)
+    for i in range(100):
+        test_acc.append(lr.test_accuracy(log_reg.w, add_dummy(X_test), Y_test))
+        train_acc.append(lr.test_accuracy(log_reg.w, add_dummy(X_train), Y_train))
+        log_reg.another_batch(add_dummy(X_train), Y_train, learn)
+
+    # 3
+    ''' The only change is the addition of the regularationzation
+        of ||w||22.  The gradient of ||w||22 is 2w.
+        See this page for proof
+        https://math.stackexchange.com/questions/883016/gradient-of-l2-norm-squared/883024
+        Of course gradient of L(w) + 1/2 ||w||22 would then be:
+        [gradient of L(w)] + w
+
+        Since the change in the objective function is deicrete in our case, the batch learning
+        algorithm represents the gradient descent:
+        w <- w - n * delta(L(w))
+        as
+        w <- w + n * d
+        where d = -delta(L(w)) and is found thru the batch iteration
+        To update this to account for L2 regularization, change the aforementioned line to:
+        w <- w - n * delta(L(w) + 1/2 lambda * ||w||22)
+        which is:
+        w <- w + n * d - n * lambda * w
+    '''
+
+    # 4
+    lams = [10**x for x in range(-3,4)]
+    test_acc = []
+    train_acc = []
+    learn = 10 ** -9
+    thresh = 100
+    for lam in lams:
+        log_reg = lr.Log_Reg(add_dummy(X_train), Y_train, 'iteration', learn, 50, lamb=lam)
+        test_acc.append(lr.test_accuracy(log_reg.w, add_dummy(X_test), Y_test))
+        train_acc.append(lr.test_accuracy(log_reg.w, add_dummy(X_train), Y_train))
+
+
+
 
 if __name__ == "__main__":
     main()
--- HW2/src/log_reg2.py
+++ HW2/src/log_reg2.py
@@ -22,23 +22,34 @@ def L(Y, X, w):
     return to_return
 
 
+def test_accuracy(w, X, Y):
+    correct = 0
+    for i in range(len(Y)):
+        prediction = sigmoid(w,X[i])
+        if ((prediction > 0.5) and (Y[i]==1)) or ((prediction < 0.5) and (Y[i]==0)):
+            correct += 1
+    return correct/len(Y)
+
 class Log_Reg(object):
-    def __init__(self, X, Y, con_key, lr, thresh):
+    def __init__(self, X, Y, con_key, lr, thresh, lamb=0):
         self.w = np.zeros(len(X[0]))
         self.thresh = thresh
         self.iter = 0
         self.norm = float("inf")
         self.obj = float("inf")
+        self.obj_ch = float("inf")
         while not self.converge(con_key):
             d = np.zeros(len(X[0]))
             for i in range(len(Y)):
                 y_hat_i = sigmoid(self.w, X[i])
                 error = Y[i] - y_hat_i
                 d = d + error * X[i]
-            self.w += lr * d
+            self.w += lr * d - lr * lamb * self.w
             self.iter += 1
             self.obj_prev = self.obj
             self.obj = L(Y, X, self.w)
+#            print(self.obj)
+            self.accuracy = test_accuracy(self.w, X, Y)
             self.obj_ch = abs(self.obj - self.obj_prev)
             self.norm = np.linalg.norm(d)
 
@@ -51,5 +62,22 @@ class Log_Reg(object):
             'iteration': self.iter >= self.thresh
         }[key]
 
+
     def predict(self,x):
         return sigmoid(self.w,x)
+
+
+    def another_batch(self, X, Y, lr):
+        d = np.zeros(len(X[0]))
+        for i in range(len(Y)):
+            y_hat_i = sigmoid(self.w, X[i])
+            error = Y[i] - y_hat_i
+            d = d + error * X[i]
+        self.w += lr * d
+        self.iter += 1
+        self.obj_prev = self.obj
+        self.obj = L(Y, X, self.w)
+        #            print(self.obj)
+        self.accuracy = test_accuracy(self.w, X, Y)
+        self.obj_ch = abs(self.obj - self.obj_prev)
+        self.norm = np.linalg.norm(d)
