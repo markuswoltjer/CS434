e08d42f19be259b323b4ec4e0d886e787a040a3c
samtjacobs <jacobssa@oregonstate.edu>
2017-04-30 13:17:49 -0700

    Fixed Bugs, see comments

--- HW3/HW3.py
+++ HW3/HW3.py
@@ -6,6 +6,13 @@ import DecTree as dt
 def csv_to_array(filename):
     return np.genfromtxt(filename, delimiter=",")
 
+def get_error(predictions, actual):
+    incorrect = 0
+    for i in range(len(predictions)):
+        if predictions[i] != actual[i]:
+            incorrect += 1
+    return incorrect/len(predictions)
+
 def main():
 
     # Load data
@@ -23,9 +30,19 @@ def main():
         knn_predictions.append(myKNN.predict(test[i], k))
     print("KNN predictions")
     print(knn_predictions)
+    # Added in print error rate
+    # Pretty high error rate, BUT < 0.5 so something is working
+    # Pro'ly issue w/ algorithm itself, not our code
+    print("Error rate: " + str(get_error(knn_predictions, test[:,0])))
+
+    # KNN modifies the data,
+    # Re-load data for the decision tree
+    test = csv_to_array("knn_test.csv")
+    train = csv_to_array("knn_train.csv")
 
     # Part II
     print("Part II\n")
+    print("NB: use python3 interpreter to work properly")
     # Problem 1
     stump = dt.DecTree(train, 1)
     print("Problem 1: \n")
