b14b39c52242cc0859b0a16234f71cef634d1d83
samtjacobs <jacobssa@oregonstate.edu>
2017-05-22 16:20:39 -0700

    Added output page

--- HW4/hac.py
+++ HW4/hac.py
@@ -1,7 +1,6 @@
 
 import numpy as np
-
-
+import operator as op
 
 class HAC:
     def __init__(self, filename):
@@ -9,7 +8,7 @@ class HAC:
         self.constructDistMat()
         #clusters exist as idxs for self.data
         self.clusters = [ [x] for x in   list(range(0, len(self.data)))]
-        print(self.clusters)
+#        print(self.clusters)
 
     def constructDistMat(self):
         #memoize all possible N^2 connections in the complete graph
@@ -40,40 +39,45 @@ class HAC:
 
         return completeDist
 
-    def singleLink(self):
-        while len(self.clusters) > 10:
+    def linkDist(self, method, x, y):
+        ops = {'single': op.lt,
+               'complete': op.gt}
+        refs = {'single': float('inf'),
+                'complete': 0}
+        dist = refs[method]
+        for a in self.clusters[x]:
+            for b in self.clusters[y]:
+                if ops[method](self.distMat[a][b], dist):
+                    dist = self.distMat[a][b]
+
+        return dist
+
+
+    def link(self, k, method):
+        while len(self.clusters) > k:
             minGroups = (-1, -1, float('inf'))
 
             #find nearest clusters
             for x in range(0, len(self.clusters)):
                 for y in range(x+1, len(self.clusters)):
-                    tmp = self.singleLinkDist(x, y)
+                    tmp = self.linkDist(method, x, y)
                     if(tmp < minGroups[2]):
                         minGroups = (x, y, tmp)
 
-            #merge clusters 
+            #merge clusters
             self.clusters[minGroups[0]] += self.clusters[minGroups[1]]
             self.clusters.remove(self.clusters[minGroups[1]])
-            print(minGroups);
+            #print(minGroups);
 
         return;
 
+
+    def singleLink(self):
+        self.link(10, 'single')
+        return;
+
     def completeLink(self):
-        while len(self.clusters) > 10:
-            minGroups = (-1, -1, float('inf'))
-
-            #find nearest clusters
-            for x in range(0, len(self.clusters)):
-                for y in range(x+1, len(self.clusters)):
-                    tmp = self.completeLinkDist(x, y)
-                    if(tmp < minGroups[2]):
-                        minGroups = (x, y, tmp)
-
-            #merge clusters 
-            self.clusters[minGroups[0]] += self.clusters[minGroups[1]]
-            self.clusters.remove(self.clusters[minGroups[1]])
-            print(minGroups);
-
+        self.link(10, 'complete')
         return;
 
 
