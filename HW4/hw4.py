29efa38dc07bbf253d0facbb50a2c897dfa8b708
ErichKramer <erich.kramer1@gmail.com>
2017-05-22 02:56:14 -0700

    I think it's working boi

---
+++ HW4/hw4.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib as plt
+
+
+import km
+
+
+
+
+
+def main():
+    
+
+    print("Generating object")
+    kTwo = km.kmean("data.txt", 2)
+
+    print("Solving kmean")
+    SSETrend = kTwo.iterSolve()
+    
+    print("Trend is: ", SSETrend)
+    print("Size is: ", len(SSETrend))
+
+
+main();
+#Each row in the data set is a 28x28 pixel representation of a digit
+#30,000 rows
+
+
+#1, implement K-means algorithm with k = 2, plot the objective (SSE) as a function of iterations.
+#Present results of a typical run.
+
+
+#pick k values randomly, 0-29999. for each value, find nearest k and store for that k
+#Find the mean of these points, and the point closes to this mean. This is the new k location
+#Iterate 
+
+
+
+#2, Apply this implementation with different values of k {3..10} For each k, please run the alg. 10 times.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ HW4/km.py
@@ -0,0 +1,72 @@
+import numpy as np
+import random
+'''
+    data file is a 764 feature set of 6000 values
+    this is the updated, previously as 30,000
+'''
+
+class kmean:
+    def __init__(self, filename, k):
+        self.data = np.genfromtxt(filename, delimiter=",")
+        self.k = k
+        #get k unique indexes in data and store them
+        self.means = [self.data[x] for x in random.sample(range(0, len(self.data)), k) ]
+
+        self.iterations = 0
+        self.SSE = float("inf")
+
+    #changes state, no return value
+    def iterate(self):
+        #new means, init to 0
+        nextMean = np.zeros((len(self.means), 784))
+        nextSize = np.zeros(len(self.means))
+        self.SSE = 0
+
+        #for each entry
+        for entry in range(0, len(self.data)):
+
+            #tuple of mean index and error value
+            best = (-1, float("inf"))
+
+            #for each mean
+            for mean in range(0, self.k):
+                dist = sum( [ (x - y)**2 for x,y in zip( self.means[mean], self.data[entry] ) ] )**.5
+                if(dist < best[1]):
+                    best = (mean, dist)
+
+            #factor this entry into it's respective mean
+            nextMean[best[0]] = [x + y for x,y in zip( nextMean[best[0]],  self.data[entry]) ]
+            nextSize[best[0]] += 1
+
+            self.SSE += dist
+
+        #reassign means using aggregate means and count of points
+        self.means = [ [ x / y for x in m] for m,y in zip(nextMean, nextSize)]
+        self.iterations += 1
+        return 0
+
+    #this funciton probably shouldn't exist
+    def iterateN(self, n):
+        iterSSE = []
+        for x in range(0, n):
+            self.iterate()
+            iterSSE += [self.SSE]
+
+        return iterSSE;
+
+
+    def iterSolve(self):
+        iterSSE = []
+        prev = -1
+        curr = 0
+
+        while prev != curr:
+            self.iterate()
+            iterSSE += [self.SSE]
+            prev = curr
+            curr = self.SSE
+
+        return iterSSE
+
+
+
